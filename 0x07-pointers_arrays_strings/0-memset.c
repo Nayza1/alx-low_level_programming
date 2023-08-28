b58d42c98e86edfc3f90ce70a734b87650982947
Nayza1 <mijenameti@gmail.com>
2023-08-28 04:42:44 -0700

    alx

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
-
  * _memset - fill a block of memory with a specific value
-
  * @s: starting address of memory to be filled
 
  * @b: the desired value
@@ -15,8 +13,7 @@
  * Return: changed array with new value for n bytes
 
  */
-
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 {
 
