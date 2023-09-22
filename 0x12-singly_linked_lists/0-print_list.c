cc3083baa785c22ed60fca61e947ae4380c125cf
Nayza1 <mijenameti@gmail.com>
2023-09-22 12:40:50 -0700

    alx

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "lists.h"
-size_t print_linked_list(struct Node* head) 
+size_t print_list(const list_t *head)
 {
-       size_t count = 0;
+	size_t count = 0;
 
-       while (head)
-       {
-             if (!head->str)
-                   printf("[0] (nil)\n");
-             else
-                printf("[%lu] %s\n", head->len, head->str);
-             head = head->next;
-             count++;
-       }
-       return count;
+	while (head)
+	{
+		if (!head->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		head = head->next;
+		count++;
+	}
+
+	return (count);
 }
